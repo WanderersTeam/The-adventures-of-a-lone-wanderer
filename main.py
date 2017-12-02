22d76b8b103a686fa36db5af0db2b4b57a79aa7c
bartekzaw1 <bartekzaw1@gmail.com>
2017-12-02 21:44:20 +0100

    Main file

---
+++ main.py
@@ -0,0 +1,10 @@
+gra = 1
+hp=50
+stamina=12
+mana=10
+def cls(): print "\n" * 100
+
+while(gra == 1):
+    cls()
+    print("HP: "+str(hp)+" Stamina: "+str(stamina)+" Mana: "+str(mana))
+    gra = int(raw_input("Gra: "))
