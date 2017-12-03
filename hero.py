ec95261920139f5e2ad2226009c761eab7d20430
bartekzaw1 <bartekzaw1@gmail.com>
2017-12-03 12:42:32 +0100

    Start of files

---
+++ hero.py
@@ -0,0 +1,3 @@
+import random, new_game
+
+att = new_game.strength + random.randrange(new_game.weapon[0],new_game.weapon[1])
---
+++ new_game.py
@@ -0,0 +1,14 @@
+import weapons, random
+
+hp = 40
+exp = 0
+lvl = 1
+strength = 10
+mana = 10
+hpPotions = 2
+manaPotions = 1
+weapon = weapons.sword
+
+att = strength + random.randrange(weapon[0],weapon[1])
+
+print(att)
