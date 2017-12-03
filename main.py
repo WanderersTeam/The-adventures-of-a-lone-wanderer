2d1dfa1cf965ac171fccbf6e7e2b24d1210ce7ba
bartekzaw1 <bartekzaw1@gmail.com>
2017-12-03 12:24:36 +0100

    Change to 3.6

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ gra = 1
 hp=50
 stamina=12
 mana=10
-def cls(): print "\n" * 100
+def cls(): print("\n" * 100)
 
 while(gra == 1):
     cls()
     print("HP: "+str(hp)+" Stamina: "+str(stamina)+" Mana: "+str(mana))
-    gra = int(raw_input("Gra: "))
+    gra = int(input("Gra: "))
