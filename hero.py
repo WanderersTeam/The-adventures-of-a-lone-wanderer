a449ba503d218a7f796c5fd3a07dd86de9058c17
azuv <alicja.m.olejniczak@gmail.com>
2017-12-04 22:43:42 +0100

    #2 Hero script

--- hero.py
+++ hero.py
@@ -1,3 +1,15 @@
-import random, new_game
+#Statistics of Main Hero for new game and saved game.
 
-att = new_game.strength + random.randrange(new_game.weapon[0],new_game.weapon[1])
+def hero_att_new():
+    attributes = {"hp" : 40, "strength" : 10, "mana" : 10}
+    inventory = {"sword" : "s_sword", "shield" : "s_shield", "armor" : "s_armor",
+                 "phealth" : "s_phealth", "pmana" : "s_mana"}
+    return attributes, inventory
+
+def hero_saved_game():
+    return 0
+
+
+show = hero_att_new()
+print(show)
+print(hero_att_new())
