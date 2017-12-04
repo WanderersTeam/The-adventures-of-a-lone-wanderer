5eef13901bc39f456b362b8966e66cdbaf028030
bartekzaw1 <bartekzaw1@gmail.com>
2017-12-04 22:57:32 +0100

    #3 Main Menu

---
+++ main_menu.py
@@ -0,0 +1,20 @@
+main_menu = 1
+
+while(main_menu == 1):
+    print("""    1. New Game
+    2. Load Game
+    3. Authors
+    4. Exit""")
+    choice = int(input("What to do? [Choose the number]:"))
+
+    if(choice == 1):
+        ##Option 1
+        main_menu=1
+    elif(choice == 2):
+        ##Option 2
+        main_menu=1
+    elif(choice == 3):
+        ##Option 3
+        main_menu=1
+    elif(choice == 4):
+        main_menu = 0
