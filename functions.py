7cb881122da489b8f69e4955edab22157445ae06
bartekzaw1 <bartekzaw1@gmail.com>
2017-12-06 21:59:41 +0100

    #4

---
+++ functions.py
@@ -0,0 +1,21 @@
+import new_game
+
+attributes = {"hp": new_game.hp, "exp": new_game.exp, "lvl": new_game.lvl,
+                  "strength": new_game.strength, "mana": new_game.mana,
+                  "hpPotions": new_game.hpPotions, "manaPotions": new_game.manaPotions,
+                  "weapon": new_game.weapon, "attack": new_game.att}
+
+def save(attributes):
+    with open("saved_game.py", "w") as file:
+        file.write("hp = " + str(attributes["hp"]) + "\n")
+        file.write("exp = " + str(attributes["exp"]) + "\n")
+        file.write("lvl = " + str(attributes["lvl"]) + "\n")
+        file.write("strength = " + str(attributes["strength"]) + "\n")
+        file.write("mana = " + str(attributes["mana"]) + "\n")
+        file.write("hpPotions = " + str(attributes["hpPotions"]) + "\n")
+        file.write("manaPotions = " + str(attributes["manaPotions"]) + "\n")
+        file.write("weapon = " + str(attributes["weapon"]) + "\n")
+        file.write("attack = " + str(attributes["attack"]) + "\n")
+        file.close()
+        
+save(attributes)
---
+++ saved_game.py
@@ -0,0 +1,9 @@
+hp = 40
+exp = 0
+lvl = 1
+strength = 10
+mana = 10
+hpPotions = 2
+manaPotions = 1
+weapon = [1, 5]
+attack = 14
