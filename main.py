d95b15d51d297d288e5bbdbba45640fb52670494
bartekzaw1 <bartekzaw1@gmail.com>
2017-12-07 20:24:41 +0100

    #7

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import main_menu, weapons, functions
+import main_menu, weapons, functions, hero
 
 main_menu.mainMenu()
+attributes = main_menu.attributes
+hero.create_new_hero()
+hero.show_attributes(attributes)
