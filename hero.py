5dbae7bd0d9ee516fa358699a03213d36d07a322
bartekzaw1 <bartekzaw1@gmail.com>
2017-12-07 20:24:16 +0100

    #2

--- hero.py
+++ hero.py
@@ -1,15 +1,9 @@
-#Statistics of Main Hero for new game and saved game.
+def create_new_hero():
+    global heroName
+    heroName = str(input("What is your name, adventurer?\n"))
+    return heroName
 
-def hero_att_new():
-    attributes = {"hp" : 40, "strength" : 10, "mana" : 10}
-    inventory = {"sword" : "s_sword", "shield" : "s_shield", "armor" : "s_armor",
-                 "phealth" : "s_phealth", "pmana" : "s_mana"}
-    return attributes, inventory
-
-def hero_saved_game():
-    return 0
-
-
-show = hero_att_new()
-print(show)
-print(hero_att_new())
+def show_attributes(attributes):
+    print("Name: " +  heroName + " | HP: " + str(attributes["hp"]) + " | EXP: " + str(attributes["exp"]) + " | LEVEL: " + str(attributes["lvl"]) +
+    "\nSTR: " + str(attributes["strength"]) + " | MANA: " + str(attributes["mana"]) + " | HP Potions: " + str(attributes["hpPotions"]) +
+    "\nMana Potions: " + str(attributes["manaPotions"]) + " | Weapon: " + str(attributes["weapon"]))
