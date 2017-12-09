fd0c9de19ae3dddcfc622fe93f9aba3174b6e930
bartekzaw1 <bartekzaw1@gmail.com>
2017-12-09 15:36:58 +0100

    #7

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-import main_menu, weapons, functions, hero
+import main_menu, weapons, functions
 
-main_menu.mainMenu()
-attributes = main_menu.attributes
-hero.create_new_hero()
-hero.show_attributes(attributes)
+main_menu.mainMenu()    #Load main menu
+attributes = main_menu.attributes   #creates variable with attributes chosen in previous function
+functions.show_attributes(attributes)   #shows all attributes of character
+functions.save(attributes)      #saves attributes to file
+
+attack = functions.attack(attributes)       #creates variable attack with  str + random number from weapon attack range
+print(attack)
+
+
+"""placeholder for function testing"""
+##while(gameOn == 1):
+##    for level in range(1,11):
+##        loadLevel(level)
+##        level=+1
+##        loadTavern()
+##    
 input()
