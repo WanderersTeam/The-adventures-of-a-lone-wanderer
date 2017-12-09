0a23a5c124468b135030f1c934a499fd397525db
bartekzaw1 <bartekzaw1@gmail.com>
2017-12-09 15:36:25 +0100

    #2
    Removal of hero script

--- hero.py
+++
@@ -1,9 +0,0 @@
-def create_new_hero():
-    global heroName
-    heroName = str(input("What is your name, adventurer?\n"))
-    return heroName
-
-def show_attributes(attributes):
-    print("Name: " +  heroName + " | HP: " + str(attributes["hp"]) + " | EXP: " + str(attributes["exp"]) + " | LEVEL: " + str(attributes["lvl"]) +
-    "\nSTR: " + str(attributes["strength"]) + " | MANA: " + str(attributes["mana"]) + " | HP Potions: " + str(attributes["hpPotions"]) +
-    "\nMana Potions: " + str(attributes["manaPotions"]) + " | Weapon: " + str(attributes["weapon"]))
