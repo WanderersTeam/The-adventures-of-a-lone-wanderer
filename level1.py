86348d73f311d5aab2f09e8b451a89e6af091a1a
azuv <alicja.m.olejniczak@gmail.com>
2017-12-14 11:00:15 +0100

    #9 level1
    first level - start

---
+++ level1.py
@@ -0,0 +1,42 @@
+import saved_game, functions, main_menu, weapons
+
+main_menu.SavedGameLoad()
+
+def level1_start():
+    print("""
+
+
+
+
+
+
+
+     _      ____  __        __  ____   _        ___
+    | |    |  __| \ \      / / |  __| | |      /   |
+    | |    | |__   \ \    / /  | |__  | |     / /| |
+    | |    |  __|   \ \  / /   |  __| | |    /_/ | |
+    | |__  | |__     \ \/ /    | |__  | |__      | |
+    |____| |____|     \__/     |____| |____|     |_|
+
+                 in inn or not in inn
+
+
+
+
+
+
+
+""")
+    input("                     press enter...")
+    line = 1
+    while line <= 50:
+        print(" ")
+        line += 1
+  
+level1_start()
+attributes = main_menu.attributes
+functions.show_attributes(attributes)
+
+
+input("Stop")
+
